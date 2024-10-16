ae62a025b31bf2a1afe5ddd3ae0085553083e6a8
Haoxiang Xu <haoxiang-xu>
2024-10-16 10:34:35 -0700

    [1016: change localhost to 3001]

--- frontend_application/component_lib_testing_application/main.js
+++ frontend_application/component_lib_testing_application/main.js
@@ -122,7 +122,7 @@ const createWindow = () => {
   });
 
   // Load the index.html of the app.
-  checkServerAndLoadURL("http://127.0.0.1:3000");
+  checkServerAndLoadURL("http://127.0.0.1:3001");
 
   // Set up the application menu
   const menu = Menu.buildFromTemplate(menuTemplate);
